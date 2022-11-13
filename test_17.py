deefa12180d21395dbd21315beb1cfbe1ac6ced5
Cardio-MAP <45173133+MaciejPMarciniak@users.noreply.github.com>
2022-11-13 21:15:10 +0100

    Remove legacy code

--- test_17.py
+++ test_17.py
@@ -2,13 +2,11 @@ from smoothahaplot import AHAPlotting
 import matplotlib.pyplot as plt
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
-import pandas as pd
 
 
 exp_strain_data = [-13, -14, -16, -20, -19, -18, -19, -23, -19, -21, -20, -20, -23, -22, -28, -25, -26]
 exp_mw_data = [1926, 1525, 1673, 2048, 2325, 2200, 2197, 2014, 1982, 2199, 2431, 2409, 2554, 2961, 2328, 2329, 2288]
-n_seg = 17
-echop = False
+
 segments_17 = ['Basal Anterior', 'Basal Anteroseptal', 'Basal Inferoseptal',
                'Basal Inferior', 'Basal Inferolateral', 'Basal Anterolateral',
                'Mid Anterior', 'Mid Anteroseptal', 'Mid Inferoseptal',
@@ -21,12 +19,12 @@ mw_dict = {k: v for (k, v) in zip(segments_17, exp_mw_data)}
 aha = AHAPlotting(values=exp_mw_data, plot_output_path='./images')
 cmap = plt.get_cmap('rainbow')
 norm = (1000, 3000)
-aha.bullseye_17_smooth(cmap=cmap, norm=norm, title='Myocardial work index', units='mmHg%', smooth_contour=False)
+aha.bullseye_smooth(cmap=cmap, norm=norm, title='Myocardial work index', units='mmHg%', smooth_contour=False)
 plt.show()
 
 aha = AHAPlotting(values=exp_strain_data, plot_output_path='./images')
 levels = MaxNLocator(nbins=12).tick_values(-30, 30)
 cmap = plt.get_cmap('seismic_r')
 norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-aha.bullseye_17_smooth(cmap=cmap, norm=norm, units='%', smooth_contour=True)
+aha.bullseye_smooth(cmap=cmap, norm=norm, units='%', smooth_contour=True)
 plt.show()
